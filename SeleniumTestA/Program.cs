db75ed33c9d39cf41936fd364dd904bdef6e3e32
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-02-01 14:49:29 +0900

    ・サンプルコード作成

--- SeleniumTestA/Program.cs
+++ SeleniumTestA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,41 @@ using System.Threading.Tasks;
 
 namespace SeleniumTestA
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            IWebDriver driver = null;
+            try
+            {
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver(System.IO.Directory.GetCurrentDirectory());
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+                IWebElement element = driver.FindElement(By.Id("lst-ib"));
+
+                element.Clear();
+                element.SendKeys("yahoo");
+
+                Console.Write("Press <Enter> to exit... ");
+                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+                element = driver.FindElement(By.Name("btnG"));
+                element.Submit();
+
+                Console.Write("Press <Enter> to exit... ");
+                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+                driver.Quit();
+
+            }
+            catch (Exception ex)
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+                throw;
+            }
         }
     }
 }
