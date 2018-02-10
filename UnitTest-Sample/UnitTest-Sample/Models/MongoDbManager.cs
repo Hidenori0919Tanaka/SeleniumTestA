119408f5481d3536fbf5ed46c1015a289ca80212
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-02-10 17:11:27 +0900

    MongoDbManager追加 MongoDB追加のためgitIgnoreファイル更新

---
+++ UnitTest-Sample/UnitTest-Sample/Models/MongoDbManager.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Authentication;
+using System.Web;
+
+namespace UnitTest_Sample.Models
+{
+    public partial class MongoDbManager
+    {
+        public MongoDbManager()
+        {
+            string connectionString = ConfigurationManager.AppSettings["MongoDBConnection"];
+
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+              new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
+        }
+    }
+}
