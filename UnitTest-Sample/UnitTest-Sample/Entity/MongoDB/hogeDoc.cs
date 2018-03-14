538a081135cc5231c82f1f6ff9e19414131dbee3
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-03-14 10:34:13 +0900

    add mongoDB CollectionTable

--- UnitTest-Sample/UnitTest-Sample/Entity/MongoDB/hogeDoc.cs
+++ UnitTest-Sample/UnitTest-Sample/Entity/MongoDB/hogeDoc.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,16 @@ namespace UnitTest_Sample.Entity.MongoDB
 {
     public class hogeDoc
     {
+        [BsonId(IdGenerator = typeof(CombGuidGenerator))]
+        public Guid Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("CreatedDate")]
+        public DateTime CreatedDate { get; set; }
     }
 }
--- UnitTest-Sample/UnitTest-Sample/Models/MongoDbManager.cs
+++ UnitTest-Sample/UnitTest-Sample/Models/MongoDbManager.cs
@@ -14,50 +14,61 @@ namespace UnitTest_Sample.Models
     {
         private MongoClient client = null;
 
-        private string mongoName = ConfigurationSettings.AppSettings["MongoDatabaseName"];
-        private string mongoUserName = ConfigurationSettings.AppSettings["MongoUsername"];
-        private string mongoPassword = ConfigurationSettings.AppSettings["MongoPassword"];
-        private string mongoPort = ConfigurationSettings.AppSettings["MongoPort"];
-        private string mongoHost = ConfigurationSettings.AppSettings["MongoHost"];
-        private string collectionName = "";
+        private string mongoName = ConfigurationManager.AppSettings["MongoDatabaseName"];
+        private string mongoUserName = ConfigurationManager.AppSettings["MongoUsername"];
+        private string mongoPassword = ConfigurationManager.AppSettings["MongoPassword"];
+        private string mongoPort = ConfigurationManager.AppSettings["MongoPort"];
+        private string mongoHost = ConfigurationManager.AppSettings["MongoHost"];
+        //private string collectionName = "";
 
         public MongoDbManager()
         {
-            //string dbConnectString = ConfigurationSettings.AppSettings["MongoDatabaseName"];
-            //string dbConnectString = ConfigurationSettings.AppSettings["MongoUsername"];
-            //string dbConnectString = ConfigurationSettings.AppSettings["MongoPassword"];
-            //string dbConnectString = ConfigurationSettings.AppSettings["MongoPort"];
-            //string dbConnectString = ConfigurationSettings.AppSettings["MongoHost"];
-
-            // MongoClientの初期化
-            var settings = new MongoClientSettings
-            {
-                Server = new MongoServerAddress(mongoHost, int.Parse(mongoPort)),
-                ServerSelectionTimeout = TimeSpan.FromSeconds(5)
-            };
-            settings.SslSettings = new SslSettings();
+            MongoClientSettings settings = new MongoClientSettings();
+            settings.Server = new MongoServerAddress(mongoHost, int.Parse(mongoPort));
             settings.UseSsl = true;
+            settings.SslSettings = new SslSettings();
             settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
 
             MongoIdentity identity = new MongoInternalIdentity(mongoName, mongoUserName);
             MongoIdentityEvidence evidence = new PasswordEvidence(mongoPassword);
 
-            settings.Credentials = new List<MongoCredential>()
-    {
-      new MongoCredential("SCRAM-SHA-1", identity, evidence)
-    };
-            this.client = new MongoClient(settings);
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+
+            var mongoClient = new MongoClient(settings);
         }
+
+        //    public MongoDbManager()
+        //    {
+        //        // MongoClientの初期化
+        //        var settings = new MongoClientSettings
+        //        {
+        //            Server = new MongoServerAddress(mongoHost, int.Parse(mongoPort)),
+        //            ServerSelectionTimeout = TimeSpan.FromSeconds(5)
+        //        };
+        //        settings.SslSettings = new SslSettings();
+        //        settings.UseSsl = true;
+        //        settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+        //        MongoIdentity identity = new MongoInternalIdentity(mongoName, mongoUserName);
+        //        MongoIdentityEvidence evidence = new PasswordEvidence(mongoPassword);
+
+        //        settings.Credentials = new List<MongoCredential>()
+        //{
+        //  new MongoCredential("SCRAM-SHA-1", identity, evidence)
+        //};
+        //        this.client = new MongoClient(settings);
+        //    }
         //public MongoDbManager()
         //{
-        //    //string connectionString = ConfigurationManager.AppSettings["MongoDBConnection"];
+        //    string connectionString = ConfigurationManager.AppSettings["MongoDBConnection"];
 
-        //    //MongoClientSettings settings = MongoClientSettings.FromUrl(
-        //    //  new MongoUrl(connectionString)
-        //    //);
-        //    //settings.SslSettings =
-        //    //  new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
-        //    //var mongoClient = new MongoClient(settings);
+        //    MongoClientSettings settings = MongoClientSettings.FromUrl(
+        //      new MongoUrl(connectionString)
+        //    );
+        //    settings.SslSettings =
+        //      new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+        //    var mongoClient = new MongoClient(settings);
         //}
 
         public async Task<bool> CreateCollection()
@@ -65,7 +76,7 @@ namespace UnitTest_Sample.Models
             IMongoDatabase mongoDatabase =
               this.client.GetDatabase(mongoName);
 
-            await mongoDatabase.CreateCollectionAsync("Collection");
+            await mongoDatabase.CreateCollectionAsync("hogeDoc");
 
             return true;
         }
